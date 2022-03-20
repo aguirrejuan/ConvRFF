feba594507b6883e4eb9a5686616f5a86502b446
aguirrejuan <jucaguirrear@unal.edu.co>
2022-03-20 10:08:04 -0500

    Addep option for regularization ConvRFF

--- models/unetConvRFF.py
+++ models/unetConvRFF.py
@@ -21,7 +21,7 @@ DefaultConvRFF = partial(ConvRFF,
 upsample = partial(layers.UpSampling2D, (2,2))
 
 
-def get_model(input_shape=(128,128,3),name='UnetConvRFF',phi_units=64,padding='SAME', cRFF=True,trainable_scale=True,kernel_size=3, trainable_W=True,**kwargs):
+def get_model(input_shape=(128,128,3),name='UnetConvRFF',phi_units=64,padding='SAME', kernel_regularizer=regularizers.l2(1e-4), cRFF=True,trainable_scale=True,kernel_size=3, trainable_W=True,**kwargs):
 
     # Encoder 
     input = layers.Input(shape=input_shape)
@@ -55,7 +55,7 @@ def get_model(input_shape=(128,128,3),name='UnetConvRFF',phi_units=64,padding='S
 
 
     scale = 16
-    x = DefaultConvRFF(phi_units,trainable_scale=trainable_scale,padding=padding,kernel_size=kernel_size, trainable_W=trainable_W)(x) if cRFF else  RFF(x,input_shape[0],input_shape[1],phi_units,scale,trainable=trainable_scale)
+    x = DefaultConvRFF(phi_units,trainable_scale=trainable_scale,padding=padding,kernel_regularizer=kernel_regularizer,kernel_size=kernel_size, trainable_W=trainable_W)(x) if cRFF else  RFF(x,input_shape[0],input_shape[1],phi_units,scale,trainable=trainable_scale)
     x = layers.Reshape((int(input_shape[0]/scale),int(input_shape[1]/scale),-1))(x)
 
     #Decoder
