c5af15600704a024a74ec2ec2bb95f996e830e31
aguirrejuan <jucaguirrear@unal.edu.co>
2022-03-23 07:15:12 -0500

    Added initializer with fixed seeds

--- models/fcnConvRFF.py
+++ models/fcnConvRFF.py
@@ -1,6 +1,7 @@
 from tensorflow.keras import layers
 from tensorflow.keras import Model
 from tensorflow.keras import regularizers
+import tensorflow as tf 
 
 from .convRFF import ConvRFF, RFF
 from functools import partial
@@ -24,6 +25,9 @@ DefaultTranspConv = partial(layers.Conv2DTranspose,
 
 
 
+def kernel_initializer(seed):
+    return tf.keras.initializers.GlorotUniform(seed=seed)
+
 def get_model(input_shape=(128,128,3),name='FCNConvRFF',kernel_regularizer=regularizers.l2(1e-4),normalization=False,phi_units=2,out_channels=1,cRFF=True,padding='SAME',kernel_size=3,trainable_scale=True, trainable_W=True,**kwargs):
 
     # Encoder 
@@ -31,28 +35,28 @@ def get_model(input_shape=(128,128,3),name='FCNConvRFF',kernel_regularizer=regul
 
     x =  layers.BatchNormalization()(input)
     
-    x =  DefaultConv2D(32)(x)
-    x =  DefaultConv2D(32)(x)
+    x =  DefaultConv2D(32,kernel_initializer=kernel_initializer(34))(x)
+    x =  DefaultConv2D(32,kernel_initializer=kernel_initializer(4))(x)
     x =  layers.BatchNormalization()(x)
     x = DefaultPooling()(x) # 128x128 -> 64x64
 
-    x =  DefaultConv2D(32)(x)
-    x =  DefaultConv2D(32)(x)
+    x =  DefaultConv2D(32,kernel_initializer=kernel_initializer(56))(x)
+    x =  DefaultConv2D(32,kernel_initializer=kernel_initializer(28))(x)
     x =  layers.BatchNormalization()(x)
     x = DefaultPooling()(x) # 64x64 -> 32x32
 
-    x =  DefaultConv2D(64)(x)
-    x =  DefaultConv2D(64)(x)
+    x =  DefaultConv2D(64,kernel_initializer=kernel_initializer(332))(x)
+    x =  DefaultConv2D(64,kernel_initializer=kernel_initializer(2))(x)
     x =  layers.BatchNormalization()(x)
     x = level_1 = DefaultPooling()(x) # 32x32 -> 16x16
 
-    x =  DefaultConv2D(128)(x)
-    x =  DefaultConv2D(128)(x)
+    x =  DefaultConv2D(128,kernel_initializer=kernel_initializer(67))(x)
+    x =  DefaultConv2D(128,kernel_initializer=kernel_initializer(89))(x)
     x =  layers.BatchNormalization()(x)
     x = level_2 = DefaultPooling()(x) # 16x16 -> 8x8
 
-    x =  DefaultConv2D(256)(x)
-    x =  DefaultConv2D(256)(x)
+    x =  DefaultConv2D(256,kernel_initializer=kernel_initializer(7))(x)
+    x =  DefaultConv2D(256,kernel_initializer=kernel_initializer(23))(x)
     x =  layers.BatchNormalization()(x)
     x =  DefaultPooling()(x) # 8x8 -> 4x4
 
@@ -70,19 +74,19 @@ def get_model(input_shape=(128,128,3),name='FCNConvRFF',kernel_regularizer=regul
 
     x = layers.Reshape((int(input_shape[0]/scale),int(input_shape[1]/scale),-1))(x)
 
-    x = level_3 = DefaultTranspConv(out_channels,kernel_size=4,use_bias=False)(x)
-    x = DefaultConv2D(out_channels,kernel_size=1,activation=None)(level_2)
+    x = level_3 = DefaultTranspConv(out_channels,kernel_size=4,use_bias=False, kernel_initializer=kernel_initializer(98))(x)
+    x = DefaultConv2D(out_channels,kernel_size=1,activation=None,kernel_initializer=kernel_initializer(75))(level_2)
 
 
     x =  layers.Add()([x,level_3])
 
     
-    x = level_4 = DefaultTranspConv(out_channels,kernel_size=4,use_bias=False)(x)
-    x = DefaultConv2D(out_channels,kernel_size=1,activation=None)(level_1)
+    x = level_4 = DefaultTranspConv(out_channels,kernel_size=4,use_bias=False,kernel_initializer=kernel_initializer(87))(x)
+    x = DefaultConv2D(out_channels,kernel_size=1,activation=None,kernel_initializer=kernel_initializer(54))(level_1)
 
     x =  layers.Add()([x,level_4])
 
-    x = DefaultTranspConv(1,kernel_size=16,strides=8,activation='sigmoid',use_bias=True)(x)
+    x = DefaultTranspConv(1,kernel_size=16,strides=8,activation='sigmoid',use_bias=True,kernel_initializer=kernel_initializer(32))(x)
 
 
     model = Model(input,x,name=name)
--- models/unetConvRFF.py
+++ models/unetConvRFF.py
@@ -2,6 +2,7 @@
 from tensorflow.keras import layers
 from tensorflow.keras import Model
 from tensorflow.keras import regularizers
+import tensorflow as tf 
 
 from .convRFF import ConvRFF, RFF
 from functools import partial
@@ -20,7 +21,8 @@ DefaultConvRFF = partial(ConvRFF,
 
 upsample = partial(layers.UpSampling2D, (2,2))
 
-
+def kernel_initializer(seed):
+    return tf.keras.initializers.GlorotUniform(seed=seed)
 
 def get_model(input_shape=(128,128,3),name='UnetConvRFF',phi_units=64,padding='SAME', normalization=False,kernel_regularizer=regularizers.l2(1e-4), cRFF=True,trainable_scale=True,kernel_size=3, trainable_W=True,**kwargs):
 
@@ -29,28 +31,28 @@ def get_model(input_shape=(128,128,3),name='UnetConvRFF',phi_units=64,padding='S
 
     x =  layers.BatchNormalization()(input)
   
-    x =  DefaultConv2D(8)(x)
+    x =  DefaultConv2D(8,kernel_initializer=kernel_initializer(34))(x)
     x =  layers.BatchNormalization()(x)
-    x = level_1 = DefaultConv2D(8)(x)
+    x = level_1 = DefaultConv2D(8,kernel_initializer=kernel_initializer(4))(x)
     x =  layers.BatchNormalization()(x)
     x = DefaultPooling()(x) # 128x128 -> 64x64
 
-    x =  DefaultConv2D(16)(x)
+    x =  DefaultConv2D(16,kernel_initializer=kernel_initializer(56))(x)
     x =  layers.BatchNormalization()(x)
-    x = level_2 = DefaultConv2D(16)(x)
+    x = level_2 = DefaultConv2D(16,kernel_initializer=kernel_initializer(32))(x)
     x =  layers.BatchNormalization()(x)
     x = DefaultPooling()(x) # 64x64 -> 32x32
 
 
-    x =  DefaultConv2D(32)(x)
+    x =  DefaultConv2D(32,kernel_initializer=kernel_initializer(87))(x)
     x =  layers.BatchNormalization()(x)
-    x = level_3 = DefaultConv2D(32)(x)
+    x = level_3 = DefaultConv2D(32,kernel_initializer=kernel_initializer(30))(x)
     x =  layers.BatchNormalization()(x)
     x = DefaultPooling()(x) # 32x32 -> 16x16
 
-    x = DefaultConv2D(64)(x)
+    x = DefaultConv2D(64,kernel_initializer=kernel_initializer(79))(x)
     x =  layers.BatchNormalization()(x)
-    x = level_4 =  DefaultConv2D(64)(x)
+    x = level_4 =  DefaultConv2D(64,kernel_initializer=kernel_initializer(81))(x)
     x =  layers.BatchNormalization()(x)
     x =  DefaultPooling()(x) # 16x16 -> 8x8
 
@@ -71,41 +73,41 @@ def get_model(input_shape=(128,128,3),name='UnetConvRFF',phi_units=64,padding='S
     x = layers.Reshape((int(input_shape[0]/scale),int(input_shape[1]/scale),-1))(x)
 
     #Decoder
-    x = DefaultConv2D(128)(x)
+    x = DefaultConv2D(128,kernel_initializer=kernel_initializer(89))(x)
     x =  layers.BatchNormalization()(x)
-    x = DefaultConv2D(128)(x)
+    x = DefaultConv2D(128,kernel_initializer=kernel_initializer(42))(x)
     x =  layers.BatchNormalization()(x)
 
     
     x = upsample()(x) # 8x8 -> 16x16
     x = layers.Concatenate()([level_4,x])
-    x = DefaultConv2D(64)(x)
+    x = DefaultConv2D(64,kernel_initializer=kernel_initializer(91))(x)
     x =  layers.BatchNormalization()(x)
-    x = DefaultConv2D(64)(x)
+    x = DefaultConv2D(64,kernel_initializer=kernel_initializer(47))(x)
     x =  layers.BatchNormalization()(x)
     
     x = upsample()(x) # 16x16 -> 32x32
     x = layers.Concatenate()([level_3,x])
-    x = DefaultConv2D(32)(x)
+    x = DefaultConv2D(32,kernel_initializer=kernel_initializer(21))(x)
     x =  layers.BatchNormalization()(x)
-    x = DefaultConv2D(32)(x)
+    x = DefaultConv2D(32,kernel_initializer=kernel_initializer(96))(x)
     x =  layers.BatchNormalization()(x)
 
     x = upsample()(x) # 32x32 -> 64x64
     x = layers.Concatenate()([level_2,x])
-    x = DefaultConv2D(16)(x)
+    x = DefaultConv2D(16,kernel_initializer=kernel_initializer(96))(x)
     x =  layers.BatchNormalization()(x)
-    x = DefaultConv2D(16)(x)
+    x = DefaultConv2D(16,kernel_initializer=kernel_initializer(98))(x)
     x =  layers.BatchNormalization()(x)
 
     x = upsample()(x) # 64x64 -> 128x128
     x = layers.Concatenate()([level_1,x])
-    x = DefaultConv2D(8)(x)
+    x = DefaultConv2D(8,kernel_initializer=kernel_initializer(35))(x)
     x =  layers.BatchNormalization()(x)
-    x = DefaultConv2D(8)(x)
+    x = DefaultConv2D(8,kernel_initializer=kernel_initializer(7))(x)
     x =  layers.BatchNormalization()(x)
 
-    x = DefaultConv2D(1,kernel_size=(1,1),activation='sigmoid')(x)
+    x = DefaultConv2D(1,kernel_size=(1,1),activation='sigmoid',kernel_initializer=kernel_initializer(42))(x)
 
     model = Model(input,x,name=name)
 
