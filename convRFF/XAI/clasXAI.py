05ecd87d797c521c18a1852519689b0822f2f366
aguirrejuan <jucaguirrear@unal.edu.co>
2022-05-14 12:31:50 -0500

    Added layer CAM

--- convRFF/XAI/clasXAI.py
+++ convRFF/XAI/clasXAI.py
@@ -3,6 +3,7 @@
 from tf_keras_vis.gradcam import Gradcam
 from tf_keras_vis.gradcam_plus_plus import GradcamPlusPlus
 from tf_keras_vis.scorecam import Scorecam
+from tf_keras_vis.layercam import Layercam
 from tf_keras_vis.utils.model_modifiers import ReplaceToLinear
 import matplotlib.pyplot as plt
 from matplotlib import cm
@@ -57,11 +58,23 @@ class Cams:
 
         self.cams['scorecam'] = cams 
         
-        
+    
+    def layerCam(self,):
+        layercam = Layercam(self.model,
+                              model_modifier=ReplaceToLinear(),
+                              clone=True)
+        cams = layercam(self.score_function,
+                            self.images,
+                            penultimate_layer=self.layer,
+                            seek_penultimate_conv_layer=False)
+
+        self.cams['layercam'] = cams 
+
     def run_cams(self,):
         self.gradCam()
         self.gradCamPlusPlus()
         self.scoreCam()
+        self.layerCam()
 
 
     def _YcOc(self,cam):
@@ -101,3 +114,4 @@ class Cams:
         for name, cam in self.cams.items():
             results[name] =  [np.mean(self.cams[name][self.labels == i],axis=0) for i in np.sort(np.unique(self.labels))]
         return results
+
