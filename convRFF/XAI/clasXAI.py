Fixed average drop

convRFF/XAI/clasXAI.py
@@ -10,14 +10,17 @@ from matplotlib import cm
 import numpy as np
 
 
+
 class Cams:
+
     def __init__(self,model,images,labels,layer):
         self.model = model
         self.images = images 
         self.labels = labels 
         self.layer = layer 
         self.cams = {}
-    
+
+
     def score_function(self,output):
         return [output[i,label] for i,label in enumerate(self.labels)]
 
@@ -33,7 +36,8 @@ class Cams:
                   seek_penultimate_conv_layer=False)
 
         self.cams['gradcam'] = cams
-    
+
+
     def gradCamPlusPlus(self,):
         gradcam = GradcamPlusPlus(self.model,
                       model_modifier=ReplaceToLinear(),
@@ -45,30 +49,30 @@ class Cams:
                   seek_penultimate_conv_layer=False)
         
         self.cams['gradcamplusplus'] = cams
-    
+
+
     def scoreCam(self,):
         scorecam = Scorecam(self.model,
-                            model_modifier=ReplaceToLinear(),
                             clone=True)
         cams = scorecam(self.score_function,
                              self.images,
                              penultimate_layer=self.layer,
                                 seek_penultimate_conv_layer=False,)
-                        #max_N=10)
 
         self.cams['scorecam'] = cams 
         
     
     def layerCam(self,):
         layercam = Layercam(self.model,
-                              model_modifier=ReplaceToLinear(),
-                              clone=True)
+                            model_modifier=ReplaceToLinear(),
+                            clone=True)
         cams = layercam(self.score_function,
-                            self.images,
-                            penultimate_layer=self.layer,
-                            seek_penultimate_conv_layer=False)
+                        self.images,
+                        penultimate_layer=self.layer,
+                        seek_penultimate_conv_layer=False)
 
         self.cams['layercam'] = cams 
+
 
     def run_cams(self,):
         self.gradCam()
@@ -86,19 +90,22 @@ class Cams:
 
     def _average_drop(self,cam):
         Y_c,O_c = self._YcOc(cam)
-        return np.mean(np.maximum(0,(Y_c-O_c))/Y_c)*100
+        return np.sum(np.maximum(0,(Y_c-O_c))/Y_c)*100
 
 
     def _average_increase(self,cam):
         Y_c,O_c = self._YcOc(cam)
         return 100*np.mean(Y_c < O_c)
 
+
     def averages_drops(self,):
         return {name:self._average_drop(cams) for name,cams in self.cams.items()}
 
+
     def averages_increases(self,):
         return {name:self._average_increase(cams) for name,cams in self.cams.items()}
-    
+
+
     def get_list_results(self):
         results = {}
         for name, cam in self.cams.items():

convRFF/XAI/segXAI.py
@@ -9,6 +9,7 @@ from tensorflow.keras.activations import sigmoid
 from tf_keras_vis.gradcam import Gradcam
 from tf_keras_vis.scorecam import Scorecam
 from tf_keras_vis.gradcam_plus_plus import GradcamPlusPlus
+from tf_keras_vis.layercam import Layercam
 from tf_keras_vis.utils.model_modifiers import ReplaceToLinear
 
 import matplotlib.pyplot as plt
@@ -83,10 +84,19 @@ class SegXAI:
 
     def scoreCam(self,):
         scorecam = Scorecam(self.model,
-                            model_modifier=ReplaceToLinear(),
                             clone=True,
                             )
         cam = scorecam(self.score_function,
+                        (self.data,self.masks),
+                        penultimate_layer=self.layer_name,
+                        seek_penultimate_conv_layer=False)
+        return cam[0]
+
+    def layerCam(self,):
+        layercam = Layercam(self.model,
+                            model_modifier=ReplaceToLinear(),
+                            clone=True)
+        cam = layercam(self.score_function,
                         (self.data,self.masks),
                         penultimate_layer=self.layer_name,
                         seek_penultimate_conv_layer=False)
@@ -102,7 +112,7 @@ class SegXAI:
 
     def average_drop(self,cam):
         Y_c,O_c = self.YcOc(cam)
-        return np.mean(np.maximum(0,(Y_c-O_c))/Y_c)*100
+        return np.sum(np.maximum(0,(Y_c-O_c))/Y_c)*100
     
 
     def average_increase(self,cam):
@@ -157,21 +167,18 @@ if __name__ == "__main__":
 
     model = load_model('/home/juan/Downloads/model.h5')
     #model = load_model('/home/juan/Documents/ConvRFF/model.h5')
-    model.summary()
-    _class = 0
+    #model.summary()
+    _class = 1
     segXAI = SegXAI(model, data,masks=masks,target_class=_class, layer_name='conv2d_33') #conv2d_33,conv2d_22
 
     cam = segXAI.gradCam()
-
     print(f'Average Drop: {segXAI.average_drop(cam):.3f} \nAverage Increace: {segXAI.average_increase(cam):.3f}')
     segXAI.plot(cam,nrows=2, ncols=5)
-
     cam = segXAI.gradCamPlusPlus()
-
     print(f'Average Drop: {segXAI.average_drop(cam):.3f} \nAverage Increace: {segXAI.average_increase(cam):.3f}')
     segXAI.plot(cam,nrows=2, ncols=5)
 
-    cam = segXAI.scoreCam()
+    cam = segXAI.layerCam()
     
     print(f'Average Drop: {segXAI.average_drop(cam):.3f} \nAverage Increace: {segXAI.average_increase(cam):.3f}')
     segXAI.plot(cam,nrows=2, ncols=5)