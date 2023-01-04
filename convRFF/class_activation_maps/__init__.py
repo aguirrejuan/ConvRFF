4c6f0d1c7703bcde1cf69eeb0eb512a275db2b00
aguirrejuan <jucaguirrear@unal.edu.co>
2023-01-04 14:55:11 -0500

    Added: function to save output model masked

--- convRFF/class_activation_maps/__init__.py
+++ convRFF/class_activation_maps/__init__.py
@@ -5,3 +5,15 @@ from .cam_data import load as load_cam_data
 
 from .output_masked import save_ouput_model_data
 from .output_masked import generator_output_model
+
+
+def load_model_from_run(run, model_name= 'model-best.h5'):
+    path = run.file(model_name).download(replace=True).name
+    model = load_model(path)
+    return model
+
+
+def get_path_run(run,root_path):
+    run_id = '_'.join([*run.path,run.config['model']])
+    folder_path = os.path.join(root_path, run_id)
+    return folder_path
