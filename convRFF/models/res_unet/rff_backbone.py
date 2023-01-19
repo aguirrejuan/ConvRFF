ea490794d461d68c34add3ed4e18da6f8e21d77d
aguirrejuan <jucaguirrear@unal.edu.co>
2023-01-19 16:38:14 -0500

    Fixed: import of rff

--- convRFF/models/res_unet/rff_backbone.py
+++ convRFF/models/res_unet/rff_backbone.py
@@ -3,7 +3,7 @@ https://github.com/cralji/RFF-Nerve-UTP/blob/main/Res-UNET_Nerve-UTP.ipynb
 """
 
 from tensorflow.keras import Model, layers, initializers
-from convRFF.models import ConvRFF_block
+from ConvRFF.layers import ConvRFF_block
 
 def kernel_initializer(seed):
     return initializers.GlorotUniform(seed=seed)
