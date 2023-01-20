29069757085a6a2ddf2f98b29cea2e528cae0bb3
aguirrejuan <jucaguirrear@unal.edu.co>
2023-01-20 15:29:06 -0500

    Added: class to storage data

--- convRFF/class_activation_maps/__init__.py
+++ convRFF/class_activation_maps/__init__.py
@@ -1,6 +1,9 @@
 from .cam_data import gen_calculate
-from .cam_data import save as save_cam_data
-from .cam_data import load as load_cam_data
+from .cam_data import save_memmap as save_cam_data
+from .cam_data import load_memmap as load_cam_data
+
+from .cam_data import save_mimic_mmap
+from .cam_data import load_mimic_mmap
 
 from .output_masked import save_ouput_model_data
 from .output_masked import generator_output_model
