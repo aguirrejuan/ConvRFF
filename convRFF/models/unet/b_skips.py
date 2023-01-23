9902e3588af0d8d8de97c8ab2ac28f3d9d596cbc
aguirrejuan <jucaguirrear@unal.edu.co>
2023-01-23 09:21:28 -0500

    Added: regularization term to unet models

--- convRFF/models/unet/b_skips.py
+++ convRFF/models/unet/b_skips.py
@@ -18,84 +18,93 @@ upsample = partial(layers.UpSampling2D, (2,2))
 def kernel_initializer(seed):
     return tf.keras.initializers.GlorotUniform(seed=seed)
 
-def get_model(input_shape=(128,128,3), name='b_skips', out_channels=1, mul_dim=1):
+def get_model(input_shape=(128,128,3), name='b_skips', out_channels=1, mul_dim=1, 
+                kernel_regularizer=None ):
 
     # Encoder 
+    k_r = kernel_regularizer#regularizers.L1L2(l1=1e-5, l2=1e-4)
     input_ = layers.Input(shape=input_shape)
 
     x =  layers.BatchNormalization(name='Batch00')(input_)
   
-    x =  DefaultConv2D(8,kernel_initializer=kernel_initializer(34),name='Conv10')(x)
+    x =  DefaultConv2D(8,kernel_initializer=kernel_initializer(34), kernel_regularizer=k_r, name='Conv10')(x)
     x =  layers.BatchNormalization(name='Batch10')(x)
-    x = level_1 = DefaultConv2D(8,kernel_initializer=kernel_initializer(4),name='Conv11')(x)
+    x = level_1 = DefaultConv2D(8,kernel_initializer=kernel_initializer(4), kernel_regularizer=k_r,name='Conv11')(x)
     x =  layers.BatchNormalization(name='Batch11')(x)
     x = DefaultPooling(name='Pool10')(x) # 128x128 -> 64x64
 
-    level_1 =  DefaultConv2D(8*mul_dim,kernel_initializer=kernel_initializer(3321),name='Conv01')(level_1)
+    level_1 =  DefaultConv2D(8*mul_dim,kernel_initializer=kernel_initializer(3321),kernel_regularizer=k_r,name='Conv01')(level_1)
 
-    x =  DefaultConv2D(16,kernel_initializer=kernel_initializer(56),name='Conv20')(x)
+    x =  DefaultConv2D(16,kernel_initializer=kernel_initializer(56),kernel_regularizer=k_r,name='Conv20')(x)
     x =  layers.BatchNormalization(name='Batch20')(x)
-    x = level_2 = DefaultConv2D(16,kernel_initializer=kernel_initializer(32),name='Conv21')(x)
+    x = level_2 = DefaultConv2D(16,kernel_initializer=kernel_initializer(32), kernel_regularizer=k_r, name='Conv21')(x)
     x =  layers.BatchNormalization(name='Batch22')(x)
     x = DefaultPooling(name='Pool20')(x) # 64x64 -> 32x32
 
-    level_2 =  DefaultConv2D(16*mul_dim,kernel_initializer=kernel_initializer(321),name='Conv02')(level_2)
+    level_2 =  DefaultConv2D(16*mul_dim,kernel_initializer=kernel_initializer(321),kernel_regularizer=k_r,name='Conv02')(level_2)
 
-    x =  DefaultConv2D(32,kernel_initializer=kernel_initializer(87),name='Conv30')(x)
+    x =  DefaultConv2D(32,kernel_initializer=kernel_initializer(87),kernel_regularizer=k_r,name='Conv30')(x)
     x =  layers.BatchNormalization(name='Batch30')(x)
-    x = level_3 = DefaultConv2D(32,kernel_initializer=kernel_initializer(30),name='Conv31')(x)
+    x = level_3 = DefaultConv2D(32,kernel_initializer=kernel_initializer(30),kernel_regularizer=k_r,name='Conv31')(x)
     x =  layers.BatchNormalization(name='Batch31')(x)
     x = DefaultPooling(name='Pool30')(x) # 32x32 -> 16x16
 
-    level_3 =  DefaultConv2D(32*mul_dim,kernel_initializer=kernel_initializer(32211),name='Conv03')(level_3)
+    level_3 =  DefaultConv2D(32*mul_dim,kernel_initializer=kernel_initializer(32211),kernel_regularizer=k_r,name='Conv03')(level_3)
 
-    x = DefaultConv2D(64,kernel_initializer=kernel_initializer(79),name='Conv40')(x)
+    x = DefaultConv2D(64,kernel_initializer=kernel_initializer(79),kernel_regularizer=k_r, name='Conv40')(x)
     x =  layers.BatchNormalization(name='Batch40')(x)
-    x = level_4 =  DefaultConv2D(64,kernel_initializer=kernel_initializer(81),name='Conv41')(x)
+    x = level_4 =  DefaultConv2D(64,kernel_initializer=kernel_initializer(81),kernel_regularizer=k_r, name='Conv41')(x)
     x =  layers.BatchNormalization(name='Batch41')(x)
     x =  DefaultPooling(name='Pool40')(x) # 16x16 -> 8x8
 
-    level_4 =  DefaultConv2D(64*mul_dim,kernel_initializer=kernel_initializer(321),name='Conv04')(level_4)
+    level_4 =  DefaultConv2D(64*mul_dim,kernel_initializer=kernel_initializer(321),kernel_regularizer=k_r, name='Conv04')(level_4)
 
     #Decoder
-    x = DefaultConv2D(128,kernel_initializer=kernel_initializer(89),name='Conv50')(x)
+    x = DefaultConv2D(128,kernel_initializer=kernel_initializer(89), kernel_regularizer=k_r,name='Conv50')(x)
     x =  layers.BatchNormalization(name='Batch50')(x)
-    x = DefaultConv2D(128,kernel_initializer=kernel_initializer(42),name='Conv51')(x)
+    x = DefaultConv2D(128,kernel_initializer=kernel_initializer(42), kernel_regularizer=k_r,name='Conv51')(x)
     x =  layers.BatchNormalization(name='Batch51')(x)
 
     
     x = upsample(name='Up60')(x) # 8x8 -> 16x16
     x = layers.Concatenate()([level_4,x])
-    x = DefaultConv2D(64,kernel_initializer=kernel_initializer(91),name='Conv60')(x)
+    x = DefaultConv2D(64,kernel_initializer=kernel_initializer(91),kernel_regularizer=k_r,name='Conv60')(x)
     x =  layers.BatchNormalization(name='Batch60')(x)
-    x = DefaultConv2D(64,kernel_initializer=kernel_initializer(47),name='Conv61')(x)
+    x = DefaultConv2D(64,kernel_initializer=kernel_initializer(47),kernel_regularizer=k_r,name='Conv61')(x)
     x =  layers.BatchNormalization(name='Batch61')(x)
     
     x = upsample(name='Up70')(x) # 16x16 -> 32x32
     x = layers.Concatenate()([level_3,x])
-    x = DefaultConv2D(32,kernel_initializer=kernel_initializer(21),name='Conv70')(x)
+    x = DefaultConv2D(32,kernel_initializer=kernel_initializer(21),kernel_regularizer=k_r,name='Conv70')(x)
     x =  layers.BatchNormalization(name='Batch70')(x)
-    x = DefaultConv2D(32,kernel_initializer=kernel_initializer(96),name='Conv71')(x)
+    x = DefaultConv2D(32,kernel_initializer=kernel_initializer(96),kernel_regularizer=k_r,name='Conv71')(x)
     x =  layers.BatchNormalization(name='Batch71')(x)
 
     x = upsample(name='Up80')(x) # 32x32 -> 64x64
     x = layers.Concatenate()([level_2,x])
-    x = DefaultConv2D(16,kernel_initializer=kernel_initializer(96),name='Conv80')(x)
+    x = DefaultConv2D(16,kernel_initializer=kernel_initializer(96),kernel_regularizer=k_r,name='Conv80')(x)
     x =  layers.BatchNormalization(name='Batch80')(x)
-    x = DefaultConv2D(16,kernel_initializer=kernel_initializer(98),name='Conv81')(x)
+    x = DefaultConv2D(16,kernel_initializer=kernel_initializer(98),kernel_regularizer=k_r,name='Conv81')(x)
     x =  layers.BatchNormalization(name='Batch81')(x)
 
     x = upsample(name='Up90')(x) # 64x64 -> 128x128
     x = layers.Concatenate()([level_1,x])
-    x = DefaultConv2D(8,kernel_initializer=kernel_initializer(35),name='Conv90')(x)
+    x = DefaultConv2D(8,kernel_initializer=kernel_initializer(35),kernel_regularizer=k_r,name='Conv90')(x)
     x =  layers.BatchNormalization(name='Batch90')(x)
-    x = DefaultConv2D(8,kernel_initializer=kernel_initializer(7),name='Conv91')(x)
+    x = DefaultConv2D(8,kernel_initializer=kernel_initializer(7),kernel_regularizer=k_r,name='Conv91')(x)
     x =  layers.BatchNormalization(name='Batch91')(x)
 
     x = DefaultConv2D(out_channels,kernel_size=(1,1),activation='sigmoid',
-                        kernel_initializer=kernel_initializer(42),
+                        kernel_initializer=kernel_initializer(42),kernel_regularizer=k_r,
                         name='Conv100')(x)
 
     model = Model(input_, x, name=name)
 
     return model 
+
+
+if __name__ == "__main__":
+    kernel_regularizer = regularizers.L1L2(l1=1e-5, l2=1e-4)
+    model = get_model(kernel_regularizer=kernel_regularizer)
+
+    model.summary()
--- convRFF/models/unet/rff_backbone.py
+++ convRFF/models/unet/rff_backbone.py
@@ -62,42 +62,42 @@ def get_model(input_shape=(128,128,3), name='rff_skips',
     level_4 = ConvRFF_block(level_4,deepth=64,name='04',kernel_regularizer=k_r,**kwargs_convrff)
 
     #Decoder
-    x = DefaultConv2D(128,kernel_initializer=kernel_initializer(89),name='Conv50')(x)
+    x = DefaultConv2D(128,kernel_initializer=kernel_initializer(89),kernel_regularizer=k_r,name='Conv50')(x)
     x =  layers.BatchNormalization(name='Batch50')(x)
-    x = DefaultConv2D(128,kernel_initializer=kernel_initializer(42),name='Conv51')(x)
+    x = DefaultConv2D(128,kernel_initializer=kernel_initializer(42),kernel_regularizer=k_r,name='Conv51')(x)
     x =  layers.BatchNormalization(name='Batch51')(x)
 
     
     x = upsample(name='Up60')(x) # 8x8 -> 16x16
     x = layers.Concatenate()([level_4,x])
-    x = DefaultConv2D(64,kernel_initializer=kernel_initializer(91),name='Conv60')(x)
+    x = DefaultConv2D(64,kernel_initializer=kernel_initializer(91),kernel_regularizer=k_r,name='Conv60')(x)
     x =  layers.BatchNormalization(name='Batch60')(x)
-    x = DefaultConv2D(64,kernel_initializer=kernel_initializer(47),name='Conv61')(x)
+    x = DefaultConv2D(64,kernel_initializer=kernel_initializer(47),kernel_regularizer=k_r,name='Conv61')(x)
     x =  layers.BatchNormalization(name='Batch61')(x)
     
     x = upsample(name='Up70')(x) # 16x16 -> 32x32
     x = layers.Concatenate()([level_3,x])
-    x = DefaultConv2D(32,kernel_initializer=kernel_initializer(21),name='Conv70')(x)
+    x = DefaultConv2D(32,kernel_initializer=kernel_initializer(21),kernel_regularizer=k_r,name='Conv70')(x)
     x =  layers.BatchNormalization(name='Batch70')(x)
-    x = DefaultConv2D(32,kernel_initializer=kernel_initializer(96),name='Conv71')(x)
+    x = DefaultConv2D(32,kernel_initializer=kernel_initializer(96),kernel_regularizer=k_r,name='Conv71')(x)
     x =  layers.BatchNormalization(name='Batch71')(x)
 
     x = upsample(name='Up80')(x) # 32x32 -> 64x64
     x = layers.Concatenate()([level_2,x])
-    x = DefaultConv2D(16,kernel_initializer=kernel_initializer(96),name='Conv80')(x)
+    x = DefaultConv2D(16,kernel_initializer=kernel_initializer(96),kernel_regularizer=k_r,name='Conv80')(x)
     x =  layers.BatchNormalization(name='Batch80')(x)
-    x = DefaultConv2D(16,kernel_initializer=kernel_initializer(98),name='Conv81')(x)
+    x = DefaultConv2D(16,kernel_initializer=kernel_initializer(98),kernel_regularizer=k_r,name='Conv81')(x)
     x =  layers.BatchNormalization(name='Batch81')(x)
 
     x = upsample(name='Up90')(x) # 64x64 -> 128x128
     x = layers.Concatenate()([level_1,x])
-    x = DefaultConv2D(8,kernel_initializer=kernel_initializer(35),name='Conv90')(x)
+    x = DefaultConv2D(8,kernel_initializer=kernel_initializer(35),kernel_regularizer=k_r,name='Conv90')(x)
     x =  layers.BatchNormalization(name='Batch90')(x)
-    x = DefaultConv2D(8,kernel_initializer=kernel_initializer(7),name='Conv91')(x)
+    x = DefaultConv2D(8,kernel_initializer=kernel_initializer(7),kernel_regularizer=k_r,name='Conv91')(x)
     x =  layers.BatchNormalization(name='Batch91')(x)
 
     x = DefaultConv2D(out_channels,kernel_size=(1,1),activation='sigmoid',
-                        kernel_initializer=kernel_initializer(42),
+                        kernel_initializer=kernel_initializer(42),kernel_regularizer=k_r,
                         name='Conv100')(x)
 
     model = Model(input_,x,name=name)
